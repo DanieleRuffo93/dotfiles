ba160ce75c548749c3148a153c8848b9bec210f2
Daniele <daniele.ruffo93@gmail.com>
2026-03-24 21:55:32 +0100

    chore: move diagnostics config into option.lua and clean structure

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,15 +1,6 @@
-vim.diagnostic.config {
-  update_in_insert = false,
-  severity_sort = true,
-  float = { border = 'rounded', source = 'if_many' },
-  underline = { severity = vim.diagnostic.severity.ERROR },
-  virtual_text = true,
-  virtual_lines = false,
-  jump = { float = true },
-}
-
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
+
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,27 +1,48 @@
+-- Leader keys
+-- NOTE: this must be first
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-vim.g.have_nerd_font = false
 
+-- UI
+vim.g.have_nerd_font = false
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.mouse = 'a'
 vim.o.showmode = false
+vim.o.signcolumn = 'yes'
+vim.o.cursorline = true
+vim.o.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
-vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
-
+-- Editing
 vim.o.breakindent = true
 vim.o.undofile = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
-vim.o.signcolumn = 'yes'
+vim.o.confirm = true
+vim.o.inccommand = 'split'
+
+-- Performance
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
+
+-- Splits
 vim.o.splitright = true
 vim.o.splitbelow = true
-vim.o.list = true
-vim.o.inccommand = 'split'
-vim.o.cursorline = true
-vim.o.scrolloff = 10
-vim.o.confirm = true
 
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- Scrolling
+vim.o.scrolloff = 10
+
+-- Clipboard
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+
+-- Diagnostics
+vim.diagnostic.config {
+  update_in_insert = false,
+  severity_sort = true,
+  float = { border = 'rounded', source = 'if_many' },
+  underline = { severity = vim.diagnostic.severity.ERROR },
+  virtual_text = true,
+  virtual_lines = false,
+  jump = { float = true },
+}
+
