577af69493062339c50125974d999e385583ec81
Daniele <daniele.ruffo93@gmail.com>
2026-03-28 00:11:51 +0100

    feat: add simple menu

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -18,5 +18,20 @@ return {
     local statusline = require 'mini.statusline'
     statusline.setup { use_icons = vim.g.have_nerd_font }
     statusline.section_location = function() return '%2l:%-2v' end
+
+    local starter = require 'mini.starter'
+    starter.setup {
+      header = 'Welcome back, Daniele',
+      items = {
+        starter.sections.recent_files(5, true),
+        {
+          { name = 'Find file', action = 'Telescope find_files', section = 'Actions' },
+          { name = 'Live grep', action = 'Telescope live_grep', section = 'Actions' },
+          { name = 'New file', action = 'enew', section = 'Actions' },
+          { name = 'Quit', action = 'qa', section = 'Actions' },
+        },
+      },
+      footer = '',
+    }
   end,
 }
