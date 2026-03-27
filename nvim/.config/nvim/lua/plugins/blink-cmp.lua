e7d5290a81adf29cdc292de771a8f109aae3c83b
Daniele <daniele.ruffo93@gmail.com>
2026-03-27 22:11:20 +0100

    chore: enable blink-cmp documentation auto show

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -21,7 +21,7 @@ return {
       nerd_font_variant = 'mono',
     },
     completion = {
-      documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      documentation = { auto_show = true, auto_show_delay_ms = 500 },
     },
     sources = {
       default = { 'lsp', 'path', 'snippets' },
@@ -31,3 +31,4 @@ return {
     signature = { enabled = true },
   },
 }
+
