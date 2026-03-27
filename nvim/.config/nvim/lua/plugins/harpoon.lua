3aaee1057d359734cbcdbe48e75132551ca9d9bd
Daniele <daniele.ruffo93@gmail.com>
2026-03-27 01:23:51 +0100

    refactor: update harpoon navigation keys to use numbers

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -2,10 +2,11 @@ return {
   'ThePrimeagen/harpoon',
   keys = {
     { '<leader>ha', function() require('harpoon.mark').add_file() end, desc = '[A]dd file' },
-    { '<leader>he', function() require('harpoon.ui').toggle_quick_menu() end, desc = '[e]dit list' },
-    { '<leader>hs', function() require('harpoon.ui').nav_file(1) end, desc = '[1] file' },
-    { '<leader>hd', function() require('harpoon.ui').nav_file(2) end, desc = '[2] file' },
-    { '<leader>hf', function() require('harpoon.ui').nav_file(3) end, desc = '[3] file' },
-    { '<leader>hg', function() require('harpoon.ui').nav_file(4) end, desc = '[4] file' },
+    { '<leader>he', function() require('harpoon.ui').toggle_quick_menu() end, desc = '[E]dit list' },
+    { '<leader>h1', function() require('harpoon.ui').nav_file(1) end, desc = 'File [1]' },
+    { '<leader>h2', function() require('harpoon.ui').nav_file(2) end, desc = 'File [2]' },
+    { '<leader>h3', function() require('harpoon.ui').nav_file(3) end, desc = 'File [3]' },
+    { '<leader>h4', function() require('harpoon.ui').nav_file(4) end, desc = 'File [4]' },
   },
 }
+
